d41c25b16a1086703fba601e4527532145e661bf
vdprogramm <dinhthanhvinhnb205@gmail.com>
2025-08-04 21:18:41 +0700

    day cao

--- src/main/java/com/example/bookmanagement/BookmanagementApplication.java
+++ src/main/java/com/example/bookmanagement/BookmanagementApplication.java
@@ -2,8 +2,10 @@ package com.example.bookmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.bookmanagement.entity")
 public class BookmanagementApplication {
 
 	public static void main(String[] args) {
